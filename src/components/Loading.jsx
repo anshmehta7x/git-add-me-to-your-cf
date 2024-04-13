6a19d9f172254461d564835b6f23fd0b330336d6
Tanush Golwala <tanushgolwala@gmail.com>
2024-04-14 02:05:37 +0530

    feat : loader

---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import loading from '../../public/loading.gif';
+
+export default function Loading() {
+    return (
+        <>
+            <div className='flex flex-col items-center'>
+                <img src={loading} alt="loading" width={200} height={200}/>
+                <p className='font-bold text-3xl text-nowrap text-white mt-12'>Video Processing...</p>
+            </div>
+        </>
+    );
+}
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Loading from "../components/Loading";
 
 export default function Landing() {
   return (
