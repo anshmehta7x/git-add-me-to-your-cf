60b2a1cb31274087711dccf8e6b762b42937c7cf
TrojanHorse0918 <manan04shah@gmail.com>
2024-04-14 03:03:28 +0530

    About Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,12 @@ export default function About() {
     <>
       <main className="h-screen w-screen bg-[url(/landing-bg.png)] bg-cover ">
         <Navbar />
-        <section className="flex flex-row w-full h-[88vh] "></section>
+        <section className="flex flex-col items-center w-full h-[88vh] p-10">
+              <h1 className="text-white text-6xl mb-10">About</h1>
+              <p className="text-white w-[60vw] text-2xl bg-white bg-opacity-10 backdrop-blur-lg p-10 rounded-3xl" style={{ boxShadow: '0 0 20px rgba(0, 0, 0, 0.25)' }}>
+              We introduce an innovative system harnessing computer vision, NLP, and machine translation to detect and translate text within video frames. This technology analyzes each frame, identifies text regions, and translates them into the learner's chosen language. The translated text is overlaid onto the original video, maintaining visual context and integrity. This approach allows learners to fully engage with complex content like diagrams and equations, eliminating the need for distracting subtitles or dubbing.
+              </p>
+        </section>
       </main>
     </>
   );
